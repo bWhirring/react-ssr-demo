e110c033eaef23d401aa4af2714c33aa79101647
huhu <bianlongting@dianwoda.com>
2019-05-27 11:08:24 +0800

    optimize

--- server/ssr.js
+++ server/ssr.js
@@ -1,20 +1,25 @@
 import React from "react";
+import { StaticRouter } from "react-router-dom";
 import { renderToString, renderToStaticMarkup } from "react-dom/server";
 
 import App from "../src/App";
 
 module.exports = function(app) {
-  app.get("/", (req, res) => {
-    var content = renderToString(<App />);
-    var html = renderToStaticMarkup(
-      <html>
+  app.get("*", (req, res) => {
+    const context = {};
+    var content = renderToStaticMarkup(
+      <StaticRouter location={req.url} context={context}>
+        <App />
+      </StaticRouter>
+    );
+    console.log(renderToString(content));
+    var html = `<html>
         <head />
         <body>
-          <div id="root" dangerouslySetInnerHTML={{ __html: content }} />
-          <script src={"/bundle.js"} />
+          <div id="app">${content}</div>
+          <script src="http://localhost:8081/dist/bundle.js"></script>
         </body>
-      </html>
-    );
+      </html>`;
     res.end(html);
   });
 };
--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
-import Test from "./Test";
+import { Route, Switch, Link } from "react-router-dom";
+
+import { router } from "./router";
 
 export default class Index extends React.Component {
   render() {
     return (
       <div>
-        App
-        <Test />
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+        <Switch>
+          {router.map((route, index) => (
+            <Route key={index} path={route.path} exact component={route.main} />
+          ))}
+        </Switch>
       </div>
     );
   }
